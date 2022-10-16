c3d8c22888a4ac825f5351bfc6f9e49b836d2b83
samriches <Samuelrichesprivate@gmail.com>
2022-10-16 23:10:03 +0100

    gonna finish this tomorrow when im not falling asleep on my macbook

--- art.js
+++ art.js
@@ -3,17 +3,20 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // draws a circle at the given point
-function drawCircle(thisX, thisY) {
-    ctx.beginPath();
-    ctx.arc(thisX, thisY, 10, 0, Math.PI * 2);
+function drawCircle(thisX, thisY, radius) {
+    ctx.arc(thisX, thisY, radius, 0, Math.PI * 2);
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fill();
-    ctx.closePath();
 }
-
 // draws a triangle onto the canvas
-function drawTriangle() {
-    drawCircle(50, 50);
+function drawTriangle(a, b, posX, posY) {
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.moveTo(posX, posY);
+    ctx.lineTo(a, posY);
+    ctx.lineTo(posX, b);
+    ctx.lineTo(posX, posY);
+    ctx.stroke();
 }
 
-window.onload(drawTriangle());
+window.onload = drawTriangle(200, 200, 100, 10);
