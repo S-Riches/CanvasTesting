ea748ac1ffa746e328b00bab476583c85eeaa26d
S-Riches <samuelrichesprivate@gmail.com>
2022-10-17 19:57:48 +0100

    got triangles drawing onto the screen now, and i have an angle of approach for the tiling

--- art.js
+++ art.js
@@ -4,19 +4,29 @@ const ctx = canvas.getContext("2d");
 
 // draws a circle at the given point
 function drawCircle(thisX, thisY, radius) {
+    ctx.beginPath();
     ctx.arc(thisX, thisY, radius, 0, Math.PI * 2);
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fill();
 }
 // draws a triangle onto the canvas
-function drawTriangle(a, b, posX, posY) {
+function drawTriangle(width, height, posX, posY) {
     ctx.beginPath();
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.moveTo(posX, posY);
-    ctx.lineTo(a, posY);
-    ctx.lineTo(posX, b);
+    ctx.lineTo(posX + width, posY);
+    ctx.lineTo(posX, posY + height);
     ctx.lineTo(posX, posY);
     ctx.stroke();
+    drawCircle(posX + width, posY, width / 50);
+    drawCircle(posX, posY + height, width / 50);
+    drawCircle(posX, posY, width / 50);
 }
 
-window.onload = drawTriangle(200, 200, 100, 10);
+function tilingLoop(amountOfTriangles) {
+    // if we add to the position or set a negative value for the width, or height value we can tile quite easily, going to do some testing to see if i can get it to tile randomly
+    drawTriangle(150, 150, 500, 500);
+    drawTriangle(150, -150, 500, 500);
+}
+
+window.onload = tilingLoop(10);
